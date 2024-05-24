12372d87faa8930bc5264717db4f43e83a42b3bb
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-23 21:16:04 -0500

    Changing the status for creating the suers

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,16 +1,18 @@
 class Api::V1::ProductsController < ApplicationController
 
+  # Get all the products in with Stock
   def index
     products = Product.all
     render json: products, status: :ok
   end
 
+  # Create new Product
   def create
     product = Product.new(product_params)
     if product.save
       render json: product, status: :created
     else
-      render json: product.errors, status: :unprocessable_entity
+      render json:{status: 400, error:"invalid request"}, status: :bad_request
     end
   end
 
