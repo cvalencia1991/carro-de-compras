9780040abefda28a376030d8f5aac265cbdb0830
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-25 10:30:45 -0500

    Update configuration Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::API
     before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
     # use Rescue Form to put some errors more simples when you send a bad request or a not found API
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     rescue_from ActionController::ParameterMissing, with: :render_bad_request
     rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+    rescue_from ActionController::RoutingError, with: :render_not_found
+
+    protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
+    end
 
     private
 
@@ -19,4 +27,7 @@ class ApplicationController < ActionController::API
       render json: { error: e.message }, status: :unprocessable_entity
     end
 
+    def route_not_found
+      render json: { error: 'Routing error: No route matches the requested URL' }, status: :not_found
+    end
 end
