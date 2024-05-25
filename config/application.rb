21bbaa0ad5385202d6b2c3a5d909491dddb167f0
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-25 10:30:28 -0500

    Remove Cofiguration Cookies devise

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,5 @@ module CarroDeCompras
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore, key: 'Cart_Shop_Cookie'
   end
 end
