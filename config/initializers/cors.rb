fb5cd7bed0388100ead0ad3b2d7e6766ed8aa672
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-26 09:49:30 -0500

    Update Cors Configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"  # Change this with the domain of Front end Or Framework and the
-    resource "*", #  Resources for the routes that are avaible to use
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: [:Authorization]
+    origins '*' # later change to the domain of the frontend app
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             expose: [:Authorization]
   end
 end
