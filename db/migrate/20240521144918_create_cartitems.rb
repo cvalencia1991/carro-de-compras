37d52e4f3e6f5ae2187f2ab38eea72befbf7a2df
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-27 18:01:36 -0500

    Update Cart Items Migration

--- db/migrate/20240521144918_create_cartitems.rb
+++ db/migrate/20240521144918_create_cartitems.rb
@@ -3,6 +3,7 @@ class CreateCartitems < ActiveRecord::Migration[7.1]
     create_table :cart_items do |t|
       t.references :cart, foreign_key: true
       t.references :product, foreign_key: true
+      t.decimal :subtotal, default: 0.0
       t.integer :quantity
       t.timestamps
     end
