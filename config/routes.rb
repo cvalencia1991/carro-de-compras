ca78e5b68454f6db5fb706a7003c2dc57c1e0073
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-27 18:41:41 -0500

    Update Routes and structure for the documentation and grapql Structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  #Create documentation for the API using Rswag gem
+  # Create documentation for the API using Rswag gem
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
 
   # set Configuration to development the graphQl
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
-  post '/graphql', to: 'graphql#execute'
+  mount GraphiQL::Rails::Engine, at: '/api/v1/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
+
 
   # the health stablish to test if the server is up
   get 'up' => 'rails/health#show', as: :rails_health_check
@@ -14,12 +14,13 @@ Rails.application.routes.draw do
   # create api/v1/... routes
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :cartitem, only: %i[create update destroy]
+      resources :cart_items, only: %i[create update destroy show]
       resources :users
       resources :products, only: %i[create index]
-      resources :current_user, only: [:index]
+      resources :current_user, only: %i[index]
       resources :carts, only: %i[show update destroy]
-      #make this scope to create the sign_up and sign_in inside of the api
+      # make this scope to create the sign_up and sign_in inside of the api
+      post '/graphql', to: 'graphql#execute'
       devise_scope :user do
         post 'sign_in', to: 'sessions#create'
         post 'sign_up', to: 'registrations#create'
