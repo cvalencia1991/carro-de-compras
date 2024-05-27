fb5132358fcec98ae5f0e7bc4cc57b6870319471
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-27 18:35:03 -0500

    Correct Name of CartItems Model

---
+++ app/models/cart_item.rb
@@ -0,0 +1,9 @@
+class CartItem < ApplicationRecord
+  include CartItemConcern
+  belongs_to :cart
+  belongs_to :product
+
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+  validate :product_in_stock
+
+end
